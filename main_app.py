2b19491bfa43946c24deb18ab5a8e2964d449f99
Pedro Bacelar <pedrobacelarsantos@usp.br>
2024-04-26 12:07:51 +0100

    test

--- main_app.py
+++ main_app.py
@@ -3,5 +3,5 @@ import streamlit as st
 #--- Replicate API Settings & llama2
 #test_secret = st.secrets['test']
 st.write("Hello World")
-#test_secret = st.secrets["db_username"]
-#st.write(test_secret)
+test_secret = st.secrets["REPLICATE_API_TOKEN"]
+st.write(test_secret)
