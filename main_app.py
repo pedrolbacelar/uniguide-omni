8d0c48aeb3be0fcc7d7f2d3d8b6f6e3352eeb810
Pedro Bacelar <pedrobacelarsantos@usp.br>
2024-04-26 12:19:16 +0100

    a

--- main_app.py
+++ main_app.py
@@ -13,15 +13,6 @@ test_secret = st.secrets["REPLICATE_API_TOKEN"]
 st.write(test_secret)
 
 
-
-#--- Replicate API Settings & llama2
-#test_secret = st.secrets['test']
-st.write("Hello World")
-test_secret = st.secrets["db_username"]
-st.write(test_secret)
-
-
-
 # https://uniguide.streamlit.app/
 
 #------------------------------------------------------- Class & Functions -------------------------------------------------------
