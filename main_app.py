3a132f4d1811b627358f260b6504b0a263cd2eca
Pedro Bacelar <pedrobacelarsantos@usp.br>
2024-04-26 12:05:53 +0100

    main

---
+++ main_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+#--- Replicate API Settings & llama2
+#test_secret = st.secrets['test']
+st.write("Hello World")
+#test_secret = st.secrets["db_username"]
+#st.write(test_secret)
