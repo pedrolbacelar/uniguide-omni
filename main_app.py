61de71502b75580212d35a5842d3110d2fcdd067
bacelarmfl <p.bacelar@mflgroup.com>
2024-05-27 16:39:06 +0200

    disclarimer + buttom

--- main_app.py
+++ main_app.py
@@ -17,7 +17,7 @@ class Assistant():
         self.unimatch_questions = {
             1: "Tell me a little bit more about your hobbies!",
             2: "What fields in the school are you interested in?",
-            3: "What is your budget for studying?"
+            3: "What are your favorite subjects?",
         }
         self.unimatch_on = False
         self.unibuddy_on = False
@@ -127,7 +127,7 @@ class Llama2():
             "You only answer requests related to universities. If the topic is not this, answer: 'I'm not trained with data not related to universities.'",
         ]
 
-    def generate_llama2_response(self, prompt_input, model = "llm_13b"):
+    def generate_llama2_response(self, prompt_input, model = "llm_7b"):
         #--- Select model
         if model == "llm_13b": model = self.llm_13b
         if model == "llm_7b": model = self.llm_7b
@@ -186,7 +186,7 @@ with st.chat_message("assistant"):
     st.markdown("Welcome to the complete experience of UniGuide Chatbot 👋!")
     st.markdown("I am here to help you find the best university for you and also to explore more about the universities information! 📖")
     st.markdown("Please, if you want to find the perfect match for you, type 'UniMatch'. If you want to discover more about the universities, type 'UniBuddy'")
-
+    st.markdown("⚠️ **Disclaimer: The product is currently in the development phase. Therefore, any suggestions or recommendations provided should be taken with user discretion.** ")
 #=======================================================
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -194,147 +194,165 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 #=======================================================
 
+#--------- Check for limited message or "end" requested by the user ---------
 
+flag_end = False
+limited_messages = 10
+
+# ------------------------------------------------------- MAIN -------------------------------------------------------
 if prompt := st.chat_input("What is up?"):
-    user.print_and_add_message(prompt)
 
-    # ------------------------------------------------------- UniMtach -------------------------------------------------------
-    #========================= Introduction ==========================
-    if prompt == "UniMatch" or prompt == "unimatch":
-        assistant.print_and_add_message("Great! Let's find the perfect match for you! 🎓")
-        assistant.print_and_add_message("Please, answer the following questions to help me find the best university for you:")
+    #---- Check for limited messages
+    if len(st.session_state.messages) > limited_messages or flag_end or prompt.lower() == "end":
+        flag_end = True
+        assistant.print_and_add_message("[ERROR] You have reached the limit of messages. Please, consider buying the full version or sending feedback to us.")
+        assistant.print_and_add_message("[Buy Full Version](https://www.uniguide.it/contact)")
+        assistant.print_and_add_message("[Send Feedback](https://www.uniguide.it/contact)")
 
-        #--- Update status unimatch
-        assistant.set_unimatch_on(True)
+
+    if not flag_end:
+
+        user.print_and_add_message(prompt)
+
+        # ------------------------------------------------------- UniMtach -------------------------------------------------------
+        #========================= Introduction ==========================
+        if prompt == "UniMatch" or prompt == "unimatch":
+            assistant.print_and_add_message("Great! Let's find the perfect match for you! 🎓")
+            assistant.print_and_add_message("Please, answer the following questions to help me find the best university for you:")
+
+            #--- Update status unimatch
+            assistant.set_unimatch_on(True)
+            
+            #--- Update matching_done to False
+            with open("cache-data.json") as f:
+                data = json.load(f)
+            data["matching_done"] = False
+            data["user_profile"] = ""
+            data["user_replies_counter"] = 0
+            with open("cache-data.json", "w") as f:
+                json.dump(data, f)
+
+        if prompt == "UniBuddy" or prompt == "unibuddy":
+            assistant.print_and_add_message("Great! Let's explore more about the universities! 📚")
+            #--- Update status unibuddy
+            assistant.set_unibuddy_on(True)
+            #--- Update matching_done to True
+            with open("cache-data.json") as f:
+                data = json.load(f)
+            data["matching_done"] = True
+            with open("cache-data.json", "w") as f:
+                json.dump(data, f)
+
+        #================================================================
+
+        #========================= CHECKING ==========================
+        if assistant.get_last_user_reply() != user.get_last_reply():
+            #--- Update user replies counter
+            assistant.update_user_replies_counter()
+            assistant.set_last_user_reply(user.get_last_reply())
+            assistant.unimatch_on = True
+
+            #--- Update user profile
+            user.update_user_profile()
+        else:
+            assistant.unimatch_on = False
+        #--- check if matching is done on the cache
+        with open("cache-data.json") as f:
+            data = json.load(f)
+        if data["matching_done"] == True:
+            assistant.unimatch_on = False
+            assistant.unibuddy_on = True
+        #================================================================
         
-        #--- Update matching_done to False
-        with open("cache-data.json") as f:
-            data = json.load(f)
-        data["matching_done"] = False
-        data["user_profile"] = ""
-        data["user_replies_counter"] = 0
-        with open("cache-data.json", "w") as f:
-            json.dump(data, f)
-
-    if prompt == "UniBuddy" or prompt == "unibuddy":
-        assistant.print_and_add_message("Great! Let's explore more about the universities! 📚")
-        #--- Update status unibuddy
-        assistant.set_unibuddy_on(True)
-        #--- Update matching_done to True
-        with open("cache-data.json") as f:
-            data = json.load(f)
-        data["matching_done"] = True
-        with open("cache-data.json", "w") as f:
-            json.dump(data, f)
-
-    #================================================================
-
-    #========================= CHECKING ==========================
-    if assistant.get_last_user_reply() != user.get_last_reply():
-        #--- Update user replies counter
-        assistant.update_user_replies_counter()
-        assistant.set_last_user_reply(user.get_last_reply())
-        assistant.unimatch_on = True
-
-        #--- Update user profile
-        user.update_user_profile()
-    else:
-        assistant.unimatch_on = False
-    #--- check if matching is done on the cache
-    with open("cache-data.json") as f:
-        data = json.load(f)
-    if data["matching_done"] == True:
-        assistant.unimatch_on = False
-        assistant.unibuddy_on = True
-    #================================================================
-    
-    #============================== MATCHING ==============================
-    if assistant.check_finished_questions():
-        #--- load user profile from json
-        student_data = load_student_data()
-    
-        #--- load universities data from json
-        universities_data, universities_names = load_universities_database()
-
-        #--- Match the student data with the universities data
-        universities_similarities = match(student_data, universities_data, universities_names)
-        # universities_similarities = {uniA: 0. , uniB: 0. , uniC: 0.}
-        #--- Sort the universities based on the similarities
-        universities_similarities = dict(sorted(universities_similarities.items(), key=lambda item: item[1], reverse=True))
-
-        #------- Printing the Results -------
-        with st.spinner("Thinking..."):
-            sleep(3)
-            assistant.print_and_add_message(f"Based on your answers, I found the best university for you are 🎉")
+        #============================== MATCHING ==============================
+        if assistant.check_finished_questions():
+            #--- load user profile from json
+            student_data = load_student_data()
         
-        # Print the 5 best universities and show the similarities
-        top_5_universities = list(universities_similarities.items())[:5]
-        for uni, sim in top_5_universities:
-            assistant.print_and_add_message(f"{uni} with similarity of {int(sim*10000)/100}%")
+            #--- load universities data from json
+            universities_data, universities_names = load_universities_database()
 
-        # --- Print a bar chart with the similarities and universities
-        st.bar_chart(universities_similarities)
+            #--- Match the student data with the universities data
+            universities_similarities = match(student_data, universities_data, universities_names)
+            # universities_similarities = {uniA: 0. , uniB: 0. , uniC: 0.}
+            #--- Sort the universities based on the similarities
+            universities_similarities = dict(sorted(universities_similarities.items(), key=lambda item: item[1], reverse=True))
 
-        #--- Clean User Profile after match
-        with open("cache-data.json") as f:
-            data = json.load(f)
-        data["user_profile"] = ""
-        with open("cache-data.json", "w") as f:
-            json.dump(data, f)
-
-        #--- Update the json file with matching done
-        with open("cache-data.json") as f:
-            data = json.load(f)
-        data["matching_done"] = True
-        with open("cache-data.json", "w") as f:
-            json.dump(data, f)
-        
-        #---- User Profile Overview ----
-        # Best University
-        best_university = top_5_universities[0][0]
-        with st.chat_message("assistant"):
+            #------- Printing the Results -------
             with st.spinner("Thinking..."):
-                response = llama2.give_profile_overview(student_data, best_university)
-                placeholder = st.empty()
-                full_response = ''
-                for item in response:
-                    full_response += item
-                    placeholder.markdown(full_response)
-                placeholder.markdown(full_response)
-            message = {"role": "assistant", "content": full_response}
-            st.session_state.messages.append(message)
-        
+                sleep(3)
+                assistant.print_and_add_message(f"Based on your answers, I found the best university for you are 🎉")
+            
+            # Print the 5 best universities and show the similarities
+            top_5_universities = list(universities_similarities.items())[:5]
+            for uni, sim in top_5_universities:
+                assistant.print_and_add_message(f"{uni} with similarity of {int(sim*10000)/100}%")
 
-    #=====================================================================
+            # --- Print a bar chart with the similarities and universities
+            st.bar_chart(universities_similarities)
 
-    #============================== QUESTION ==============================
-    if assistant.unimatch_on:
-        assistant.unimatch_question()
-    #=====================================================================
+            #--- Clean User Profile after match
+            with open("cache-data.json") as f:
+                data = json.load(f)
+            data["user_profile"] = ""
+            with open("cache-data.json", "w") as f:
+                json.dump(data, f)
 
-    
-    # ------------------------------------------------------- UniBuddy -------------------------------------------------------
-
-
-    if assistant.unibuddy_on:
-
-        # TODO:
-        # - Test API from chatgpt
-        # - give an overview of the user profile and the best university, and why it is the best match
-        # - ask if the user wants to explore a specific university
-        # - use as initial prompt the data from that university and than use chatgpt
-
-        # Display assistant response in chat message container
-        if st.session_state.messages[-1]["role"] != "assistant":
+            #--- Update the json file with matching done
+            with open("cache-data.json") as f:
+                data = json.load(f)
+            data["matching_done"] = True
+            with open("cache-data.json", "w") as f:
+                json.dump(data, f)
+            
+            #---- User Profile Overview ----
+            # Best University
+            best_university = top_5_universities[0][0]
             with st.chat_message("assistant"):
                 with st.spinner("Thinking..."):
-                    response = llama2.generate_llama2_response(prompt)
+                    response = llama2.give_profile_overview(student_data, best_university)
                     placeholder = st.empty()
                     full_response = ''
                     for item in response:
                         full_response += item
                         placeholder.markdown(full_response)
                     placeholder.markdown(full_response)
-            message = {"role": "assistant", "content": full_response}
-            st.session_state.messages.append(message)
+                message = {"role": "assistant", "content": full_response}
+                st.session_state.messages.append(message)
+            
+
+        #=====================================================================
+
+        #============================== QUESTION ==============================
+        if assistant.unimatch_on:
+            assistant.unimatch_question()
+        #=====================================================================
+
+        
+        # ------------------------------------------------------- UniBuddy -------------------------------------------------------
+
+
+        if assistant.unibuddy_on:
+
+            # TODO:
+            # - Test API from chatgpt
+            # - give an overview of the user profile and the best university, and why it is the best match
+            # - ask if the user wants to explore a specific university
+            # - use as initial prompt the data from that university and than use chatgpt
+
+            # Display assistant response in chat message container
+            if st.session_state.messages[-1]["role"] != "assistant":
+                with st.chat_message("assistant"):
+                    with st.spinner("Thinking..."):
+                        response = llama2.generate_llama2_response(prompt)
+                        placeholder = st.empty()
+                        full_response = ''
+                        for item in response:
+                            full_response += item
+                            placeholder.markdown(full_response)
+                        placeholder.markdown(full_response)
+                message = {"role": "assistant", "content": full_response}
+                st.session_state.messages.append(message)
+
+        
 
